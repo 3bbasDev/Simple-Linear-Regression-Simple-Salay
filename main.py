5e36f0b95eba843d735f7b8614ae70e6a3d09b03
Abbas K. Radam <abbasabbe1995@gmail.com>
2020-12-05 14:18:17 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as pl
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
+# import Dataset from csv
+dataset = pd.read_csv('Salary_Data.csv')
+x = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# splitting dataset to training and test
+[X_train, X_test, Y_train, Y_test] = train_test_split(x, y, test_size=0.2, random_state=0)
+
+# Train simple model by LinearRegression class
+regression: object = LinearRegression()
+regression.fit(X_train, Y_train)
+
+# Predicting Test result
+Y_perdict: object = regression.predict(X_test)
+
+# Visualising Training
+plot1 = pl.figure(1)
+pl.scatter(X_train, Y_train, color='red')
+pl.plot(X_train, regression.predict(X_train), color='black')
+pl.title('Salary Vs Experience (Training)')
+pl.xlabel('Years of Experience')
+pl.ylabel('Salary')
+
+# Visualising Test
+plot2 = pl.figure(2)
+pl.scatter(X_test, Y_test, color='red')
+pl.plot(X_train, regression.predict(X_train), color='blue')
+pl.title('Salary Vs Experience (Test)')
+pl.xlabel('Years of Experience')
+pl.ylabel('Salary')
+pl.show()
